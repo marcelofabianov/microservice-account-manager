aa7bc8e355790df728e89591b1a90ce9e95c2a92
Marcelo Fabiano <marcelo.fabiano@noufiscal.com.br>
2022-04-08 17:02:46 -0300

    test: refactor CreateAccountService and add accountRegistrationInformingStatus

--- tests/Unit/Accounts/Services/CreateAccountServiceTest.php
+++ tests/Unit/Accounts/Services/CreateAccountServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Accounts\Services;
 
 use App\Accounts\Business\DefaultAccountStatusBusiness;
+use App\Accounts\Data\Enums\AccountStatusEnum;
 use App\Accounts\Services\CreateAccountService;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,7 +19,7 @@ class CreateAccountServiceTest extends TestCase
     /**
      * @test
      */
-    public function createAccount()
+    public function accountRegistrationNotReportingStatus()
     {
         $data = [
             'document' => '2345234533453',
@@ -38,6 +39,36 @@ class CreateAccountServiceTest extends TestCase
 
         $expected['id'] = 1;
         $expected['status'] = (DefaultAccountStatusBusiness::get())->value;
+        $expected['createdAt'] = $reality['createdAt'];
+        $expected['updatedAt'] = $reality['updatedAt'];
+
+        $this->assertEquals($expected, $reality);
+    }
+
+    /**
+     * @test
+     */
+    public function accountRegistrationInformingStatus()
+    {
+        $data = [
+            'document' => '2345234533453',
+            'name' => "H2O",
+            'email' => "contact@h20.com",
+            'phone' => "(73) 5734-1084",
+            'address' => "455 Arely Bypass",
+            'addressNumber' => "432",
+            'addressComplement' => "Apt. 899",
+            'status' => AccountStatusEnum::CLIENT_ACTIVE->value
+        ];
+
+        $service = new CreateAccountService();
+        $service->setData($data);
+        $reality = $service->execute();
+
+        $expected = $data;
+
+        $expected['id'] = 1;
+        $expected['status'] = AccountStatusEnum::CLIENT_ACTIVE->value;
         $expected['createdAt'] = $reality['createdAt'];
         $expected['updatedAt'] = $reality['updatedAt'];
 
