384bbad1b0210755acd93489c959b0e7df4dec53
Marcelo Fabiano <marcelo.fabiano@noufiscal.com.br>
2022-04-08 14:37:29 -0300

    test: create listOfAccountsThatContainStatusParameter Feature test

--- tests/Feature/Accounts/AccountsTest.php
+++ tests/Feature/Accounts/AccountsTest.php
@@ -73,10 +73,53 @@ class AccountsTest extends TestCase
         $this->assertEquals($expected, $received->json());
     }
 
-//    public function listOfAccountsThatContainStatusParameter()
-//    {
-//        //
-//    }
+    /**
+     * @test
+     */
+    public function listOfAccountsThatContainStatusParameter()
+    {
+        $account = Account::factory()->create(['conta_status' => 2]);
+        $account = AccountDtoTranslate::instance()->translateSourceFromDto($account->toArray());
+
+        $received = $this->get(env('API_URL') . '/accounts?status=2', $this->getHeadersAuthorization());
+
+        $expected = [
+            'type' => 'Accounts',
+            'data' => [
+                [
+                    'id' => $account->id,
+                    'name' => $account->name,
+                    'document' => $account->document,
+                    'status' => $account->status->value,
+                    'email' => $account->email,
+                    'phone' => $account->phone,
+                    'address' => $account->address,
+                    'addressNumber' => $account->addressNumber,
+                    'addressComplement' => $account->addressComplement,
+                    'createdAt' => $account->createdAt->toIso8601String(),
+                    'updatedAt' => $account->updatedAt->toIso8601String(),
+                ]
+            ],
+            'links' => [
+                'first' => route('api.accounts.index').'?page=1',
+                'last' => null,
+                'prev' => null,
+                'next' => route('api.accounts.index').'?page=2',
+            ],
+            'meta' => [
+                'current_page' => 1,
+                'from' => 1,
+                'path' => route('api.accounts.index'),
+                'per_page' => null,
+                'to' => 1
+            ]
+        ];
+
+        $received->assertOk();
+
+        $this->assertEquals($expected, $received->json());
+
+    }
 //
 //    public function listOfAccountsThatContainRelationshipParameter()
 //    {
